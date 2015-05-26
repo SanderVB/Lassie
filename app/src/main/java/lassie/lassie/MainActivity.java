40a20ada9612581d02a4db0770e8c6a139ad3b2f
KNiemeijer <K.j.niemeijer@students.uu.nl>
2015-05-26 13:08:45 +0200

    Creatie tweede activiteit (main)

--- app/src/main/java/lassie/lassie/MainActivity.java
+++ app/src/main/java/lassie/lassie/MainActivity.java
@@ -1,23 +1,30 @@
 package lassie.lassie;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 
-public class Home extends ActionBarActivity {
-    public final static String gebruikersnaammessage = Home.gebruikersnaammessage;
+public class MainActivity extends ActionBarActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
         setTitle("Lassie");
+        ToggleButton vermist = (ToggleButton) findViewById(R.id.button_vermist);
+        vermist.setChecked(true);
+        Intent intent = getIntent();
+        String emailMessage = intent.getStringExtra(Home.gebruikersnaammessage);
+        TextView email = new TextView(this);
+        email.setTextSize(40);
+        email.setText(emailMessage);
+
     }
 
     @Override
@@ -25,7 +32,7 @@ public class Home extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         // Inflate the menu items for use in the action bar
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_home, menu);
+        inflater.inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -50,13 +57,5 @@ public class Home extends ActionBarActivity {
 
     private void openSettings() {
 
-    }
-
-    public void Continue(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        EditText gebruikersnaam = (EditText) findViewById(R.id.edittext_gebruikersnaam);
-        String gebruikersnaamgegevens = gebruikersnaam.getText().toString();
-        intent.putExtra(gebruikersnaammessage, gebruikersnaamgegevens);
-        startActivity(intent);
     }
 }
